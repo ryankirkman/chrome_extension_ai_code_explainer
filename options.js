0df38d7cd49cc24b439dbf8d0b24b8101511a03f
Ryan Kirkman <ryan@ryankirkman.com>
2023-03-19 22:57:47 -0700

    Added configurable live fetch option

--- options.js
+++ options.js
@@ -4,15 +4,19 @@ document.getElementById('optionsForm').addEventListener('submit', function (even
     const model = document.getElementById('model').value;
     const maxTokens = document.getElementById('maxTokens').valueAsNumber;
     const temperature = document.getElementById('temperature').valueAsNumber;
+    const liveResponse = document.getElementById('liveResponse').checked;
 
-    chrome.storage.local.set({ apiKey, model, maxTokens, temperature }, function () {
+    chrome.storage.local.set({ apiKey, model, maxTokens, temperature, liveResponse }, function () {
         alert('Settings saved');
     });
 });
 
-chrome.storage.local.get(['apiKey', 'model', 'maxTokens', 'temperature'], function (data) {
+chrome.storage.local.get(['apiKey', 'model', 'maxTokens', 'temperature', 'liveResponse'], function (data) {
     document.getElementById('apiKey').value = data.apiKey || '';
     document.getElementById('model').value = data.model || 'gpt-3.5-turbo';
     document.getElementById('maxTokens').valueAsNumber = data.maxTokens || 500;
     document.getElementById('temperature').valueAsNumber = data.temperature || 0.5;
+    // default to true
+    if(data.liveResponse === undefined) data.liveResponse = true;
+    document.getElementById('liveResponse').checked = data.liveResponse;
 });
--- popup.js
+++ popup.js
@@ -1,27 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
     chrome.runtime.sendMessage({ action: 'getSelectedText' }, function (response) {
         document.getElementById('explanation').innerText = 'Loading...';
-        getExplanation(response.text)
-            .then((explanation) => {
-                document.getElementById('explanation').innerText = explanation;
-            })
-            .catch((error) => {
-                console.error(error);
-            });
+        getExplanation(response.text);
     });
 });
 
 function getSettings() {
     return new Promise((resolve, reject) => {
-        chrome.storage.local.get(['apiKey', 'model', 'maxTokens', 'temperature'], function (data) {
+        chrome.storage.local.get(['apiKey', 'model', 'maxTokens', 'temperature', 'liveResponse'], function (data) {
             resolve(data);
         });
     });
 }
 
 async function getExplanation(code) {
+    const outputText = document.getElementById('explanation');
     try {
-        const { apiKey, model, maxTokens, temperature } = await getSettings();
+        const { apiKey, model, maxTokens, temperature, liveResponse } = await getSettings();
         if (!apiKey) {
             throw new Error('API Key is not configured. Please configure it in the extension options.');
         }
@@ -30,36 +25,92 @@ async function getExplanation(code) {
             { role: "system", content: "You are an AI that can explain code in simple terms." },
             { role: "user", content: `Explain the following code in simple terms: ${code}` },
         ];
-
-        const response = await fetch(apiUrl, {
+        const options = {
             method: 'POST',
             headers: {
                 'Authorization': `Bearer ${apiKey}`,
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({
+            body: {
                 model: model,
                 messages: messages,
                 max_tokens: maxTokens,
                 temperature: temperature,
-            }),
-        });
+            }
+        };
 
-        if (!response.ok) {
-            throw new Error('API request failed');
+        let fetchFunction = fetchSync;
+        if (liveResponse) {
+            options.body.stream = true;
+            fetchFunction = fetchAsync;
         }
-
-        const data = await response.json();
-
-        if (data.choices && data.choices.length > 0) {
-            const aiMessage = data.choices[0].message.content;
-            return aiMessage.trim();
-        } else {
-            throw new Error('No explanation found');
-        }
+        options.body = JSON.stringify(options.body);
+        const response = await fetch(apiUrl, options);
+        console.log('attempting to fetch');
+        fetchFunction(response, outputText);
     } catch (error) {
         alert(error);
         console.error(error);
         throw error;
+    }
+}
+
+function fetchAsync(response, outputText) {
+    outputText.innerText = '';
+    const reader = response.body.getReader();
+    const decoder = new TextDecoder('utf-8');
+    let buffer = '';
+
+    async function readStream() {
+        try {
+            const { value, done } = await reader.read();
+
+            if (done) {
+                return;
+            }
+
+            buffer += decoder.decode(value, { stream: true });
+
+            while (buffer.includes('\n')) {
+                const newLineIndex = buffer.indexOf('\n');
+                const eventStr = buffer.slice(0, newLineIndex).trim();
+                buffer = buffer.slice(newLineIndex + 1);
+
+                if (eventStr.startsWith('data: ')) {
+                    const dataStr = eventStr.slice('data: '.length);
+
+                    if (dataStr === '[DONE]') {
+                        // Close the stream if the [DONE] message is received
+                        break;
+                    } else {
+                        const jsonResponse = JSON.parse(dataStr);
+                        if (jsonResponse.choices && jsonResponse.choices.length > 0 && jsonResponse.choices[0].delta.content) {
+                            outputText.innerText += jsonResponse.choices[0].delta.content;
+                        }
+                    }
+                }
+            }
+
+            readStream();
+        } catch (error) {
+            console.error('Error reading the stream:', error);
+        }
+    }
+
+    readStream();
+}
+
+async function fetchSync(response, outputText) {
+    if (!response.ok) {
+        throw new Error('API request failed');
+    }
+
+    const data = await response.json();
+
+    if (data.choices && data.choices.length > 0) {
+        const aiMessage = data.choices[0].message.content;
+        outputText.innerText = aiMessage;
+    } else {
+        throw new Error('No explanation found');
     }
 }
